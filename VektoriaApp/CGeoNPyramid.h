791a96bc8ffa383566a47b29db8d3ce5510b3f24
Philipp <philipp@geirhos.com>
2021-04-20 12:00:37 +0200

    Revert "Merge branch 'master' of https://github.com/philgei/ProzMod"
    This reverts commit 2312e87c74565a601b99c94c9aa20e877dff93fa, reversing
    changes made to 506f4e0d8c5fcf8b8297f5102ff07fc6b65221ad.

--- VektoriaApp/CGeoNPyramid.h
+++ VektoriaApp/CGeoNPyramid.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "Vektoria\Root.h"
-#include "Util.h"
 using namespace Vektoria;
 
 class CGeoNPyramid :
@@ -8,7 +7,7 @@ class CGeoNPyramid :
 {
 public:
 	CGeoNPyramid(float height, float radius);
-	CVertex m_avertexPyramid[8 * 15]; //4 is numberofCuts MUSS UNTEN AUCH GEÄNDERT WERDEN!
+	CVertex m_avertexPyramid[8 * 10]; //4 is numberofCuts MUSS UNTEN AUCH GEÄNDERT WERDEN!
 
 	int index = 0;
 	CHVector vTangent = CHVector(0.0f, 1.0f, 0.0f, 0.0f);
@@ -19,12 +18,11 @@ private:
 	CHVector bot;
 	void initTopBot();
 	void AddGroundAndFace(CHVector vec1, CHVector vec2);
-	void VertexAdd(CHVector pos, CHVector normal);
+	void VertexAdd(CHVector pos);
 
-	Util util;
 	float height;
 	float radius;
-	int numberOfCuts = 15; // HIER AUCH ÄNDERN
+	int numberOfCuts = 10; // HIER AUCH ÄNDERN
 	CHVector vecLeft;
 	CHVector vecRight;
 
